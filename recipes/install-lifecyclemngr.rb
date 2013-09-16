e80013ff740b7bfcb629b2d5d7bbc5c13c4b32cd
i806857 <dan.mount@sap.com>
2013-09-16 16:34:20 -0400

    install host agent seperately, prevent possible error with host agent pse

--- recipes/install-lifecyclemngr.rb
+++ recipes/install-lifecyclemngr.rb
@@ -8,6 +8,34 @@ if !File.exists?("#{node['hana']['installpath']}/#{node['hana']['sid']}/HLM/hlmc
 	# HANA must exist before installing HLM
 	include_recipe 'hana::install'
 	
+	# Install host agent 7.20 (patch level >= 153)
+	directory "Create temporary directory" do
+    path "#{node['install']['tempdir']}"
+    action :create
+    recursive true
+  end
+
+  execute "Get SAPCAR tool for Hana extracting hana package" do
+    cwd "#{node['install']['tempdir']}"
+    command "wget #{node['install']['files']['sapcar']} -O sapcar" #must not be named SAPCAR as it exists in archive
+  end
+
+  execute "Get SAP Host Agent package" do
+    cwd "#{node['install']['tempdir']}"
+    command "wget #{node['install']['files']['saphostagent']} -O SAP_HOST_AGENT.SAR"
+  end
+
+  execute "Extract & Install SAP Host Agent" do
+    cwd "#{node['install']['tempdir']}"
+    command "chmod +x sapcar && ./sapcar -xvf SAP_HOST_AGENT.SAR && ./saphostexec -install"
+  end
+
+  directory "Delete temporary directory" do
+    path "#{node['install']['tempdir']}"
+    recursive true
+    action :delete
+  end
+	
 	# Make SAR files available to installer as HLM stores a copy for use in remote installs
 	directory "Create archive directory" do
     path "#{node['install']['tempdir']}/archives"
@@ -15,17 +43,12 @@ if !File.exists?("#{node['hana']['installpath']}/#{node['hana']['sid']}/HLM/hlmc
     recursive true
   end
 
-  execute "Get SAPHOSTAGENT archive" do
-    cwd "#{node['install']['tempdir']}/archives"
-    command "wget #{node['install']['files']['saphostagent']} -O SAPHOSTAGENT.SAR"
-  end
-
-  execute "Get Hana lifecycle manager archive" do
+  execute "Get HANA lifecycle manager archive" do
     cwd "#{node['install']['tempdir']}/archives"
     command "wget #{node['install']['files']['hanalifecyclemngr']} -O SAPHANALM.SAR"
   end
 	
-  hana_install_command = "./hdbinst --batch --sid=#{node['hana']['sid']} --hlm_archive=#{node['install']['tempdir']}/archives/SAPHANALM.SAR --host_agent_package=#{node['install']['tempdir']}/archives/SAPHOSTAGENT.SAR"
+  hana_install_command = "./hdbinst --batch --sid=#{node['hana']['sid']} --hlm_archive=#{node['install']['tempdir']}/archives/SAPHANALM.SAR"
 
   hdbcmd "run install of hana lifecycle manager" do
     exe hana_install_command
