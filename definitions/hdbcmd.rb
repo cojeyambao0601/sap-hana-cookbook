5a8c0b281f99f44dae94e82db6424498eca4a34c
d031121 <martin.schroeder@sap.com>
2014-05-19 07:58:23 +0200

    get SAR file via remote_file if file can be access via normal file system

--- definitions/hdbcmd.rb
+++ definitions/hdbcmd.rb
@@ -11,9 +11,17 @@ define :hdbcmd, :exe => "", :bin_dir => "", :bin_file_url => "" do
     command "wget #{node['install']['files']['sapcar']}"
   end
 
-  execute "Get Hana binary package" do
-    cwd "#{node['install']['tempdir']}"
-    command "wget #{params[:bin_file_url]} -O SAP_HANA_PACKAGE.SAR"
+  #check if file can be access via filesystem, if yes copy instead of wget
+  if ::File.file?(#{params[:bin_file_url]})
+    remote_file "Get SAP_HANA_PACKAGE.SAR file" do
+      source "#{params[:bin_file_url]}"
+      path "#{node['install']['tempdir']}/SAP_HANA_PACKAGE.SAR"
+    end
+  else
+    execute "Get Hana binary package" do
+      cwd "#{node['install']['tempdir']}"
+      command "wget #{params[:bin_file_url]} -O SAP_HANA_PACKAGE.SAR"
+    end
   end
 
   execute "Extract Hana binary package" do
