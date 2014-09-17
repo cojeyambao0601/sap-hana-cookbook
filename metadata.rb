14f07b1860f927ca925e797efdb81769f8f49fe1
Boris Komraz <boris.komraz@sap.com>
2014-09-17 18:38:57 +0300

    Adding hana_client definition that updates client only if update is required and uses sap_media for media download

--- metadata.rb
+++ metadata.rb
@@ -12,4 +12,6 @@ recipe           "hana::upgrade", "Upgrades an existing SAP Hana installation"
 recipe           "hana::upgrade-client", "Upgrades an existing SAP Hana client installation"
 
 depends          "nfs"
+depend           "sapinst"
+
 supports         "suse"
