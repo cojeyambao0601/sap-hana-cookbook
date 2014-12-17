8c640ea6513a03a8fc62f7aac22e45458478f378
Andreas Bloemeke <andreas.bloemeke@sap.com>
2014-12-17 10:53:06 +0100

    bumped to 2.2.34

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "andreas.bloemeke@sap.com, thomas.graichen@sap.com, harald.kuer
 license          ""
 description      "Install/upgrade SAP Hana and SAP Hana client"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.02.33"
+version          "2.02.34"
 recipe           "hana::install", "Installs a vanilla SAP Hana on the node"
 recipe           "hana::install-worker", "Installs a vanilla SAP Hana worker on the node"
 recipe           "hana::install-client", "Installs SAP Hana client on the node"
