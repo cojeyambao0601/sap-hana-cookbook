f1d6d54237b27a807824c15217a3d1c33a4e1f92
Haggai Philip Zagury <haggai.zagury@sap.com>
2015-11-16 10:49:18 +0200

    Adding / Restoring HANAHA attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -78,3 +78,14 @@ default['install']['files']['afl_sal']              = "http://moo-repo.wdf.sap.c
 default['install']['files']['datasetsources']       = "http://moo-repo.wdf.sap.corp:8080/static/ssc/SAP/Dataset/"
 default['install']['files']['datasetnames']         = ["LUMIRA.zip"]
 default['install']['files']['deletedatasetsources'] = true
+
+
+# HANA HA
+default[:hana][:ha][:repo_baseurl]        = 'http://moo-repo.wdf.sap.corp:8080/mrepo/suse/11/'
+default[:hana][:ha][:repo_paths]          = [ "SLE11-HAE-SP3-Pool/sle-11-x86_64/SLE11-HAE-SP3-Pool", "SLE11-HAE-SP3-Updates/sle-11-x86_64/SLE11-HAE-SP3-Updates"]
+default[:hana][:ha][:packadges]           = [ "corosync","hawk","sle-hae-release","pssh","cmirrord","sleha-bootstrap","conntrack-tools","crmsh","python-dateutil","ldirectord","ocfs2-tools","csync2","openais","ctdb","drbd","yast2-cluster","python-pssh","yast2-drbd","yast2-iplb","pacemaker","pacemaker-mgmt","pacemaker-mgmt-client","release-notes-hae","resource-agents","lvm2-clvm" ]
+
+
+default[:hana][:ha][:custom_repo_baseurl] = 'http://moo-repo.wdf.sap.corp:8080/static/monsoon/sap/hana-ha/'
+default[:hana][:ha][:custom_packadges]    = [ "SAPHanaSR-0.148-0.7.1.noarch.rpm", "SAPHanaSR-doc-0.148-0.7.1.noarch.rpm" ]
+default[:hana][:ha][:tags]                = ["hana_ha_master1", "hana_ha_master2"]
