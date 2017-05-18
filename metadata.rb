bcc31326ec5bd7a2dd7dc25ef853228fc35d3a48
Yambao <coje.yambao@sap.com>
2017-05-18 11:40:05 +0800

    test git push

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'haggai.zagury@sap.com'
 license          'Apache 2.0'
 description      'Install/upgrade SAP Hana and SAP Hana client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 recipe           'hana::install', 'Installs a vanilla SAP Hana on the node'
 recipe           'hana::install-worker', 'Installs a vanilla SAP Hana worker on the node'
 recipe           'hana::install-client', 'Installs SAP Hana client on the node'
